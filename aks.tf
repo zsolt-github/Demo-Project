d6e4f8af4169fa11ac185756f8f01eaa9c9079c0
zsolt-github <ZF.Cloud@outlook.com>
2022-10-04 21:58:15 +0100

    Added the namespace-dev.yaml, namespace-prod.yaml, pod.yaml and replicaset.yaml files

--- aks.tf
+++ aks.tf
@@ -101,12 +101,6 @@ resource "local_file" "aks_kubeconfig" {
 }
 
 /*
-resource "azurerm_role_assignment" "role_acrpull" {
-  scope                            = azurerm_container_registry.acr.id
-  role_definition_name             = "AcrPull"
-  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
-  skip_service_principal_aad_check = true
-}
 
 resource "azurerm_container_registry" "acr" {
   name                = var.acr_name
@@ -120,4 +114,13 @@ resource "azurerm_container_registry" "acr" {
     "environment"  = "test"
   }
 }
+
+resource "azurerm_role_assignment" "role_acrpull" {
+  scope                            = azurerm_container_registry.acr.id
+  role_definition_name             = "AcrPull"
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity.0.object_id
+  skip_service_principal_aad_check = true
+}
+
+
 */
--- terraform.tfvars
+++ terraform.tfvars
@@ -11,17 +11,17 @@ az_subnet_address_prefix = "10.30.1.0/24"
 
 # AKS
 aks_cluster_name = "AKS-P2-TF"
-aks_version = "1.23.8"
+aks_version = "1.24"
 #1.24.3
 
 #    Pool name must start with a lowercase letter, have max length of 12, and only have characters a-z0-9
 aks_default_node_name = "system"
 aks_default_node_count = 1
-aks_default_node_vm_size = "Standard_DS2_v2"
+aks_default_node_vm_size = "Standard_B2s"
 aks_default_node_node_type = "VirtualMachineScaleSets"
 
 aks_worker_pool_1_name = "workerpool1"
-aks_worker_pool_1_vm_size = "Standard_DS2_v2"
+aks_worker_pool_1_vm_size = "Standard_B2s"
 aks_worker_pool_1_node_type = "VirtualMachineScaleSets"
 aks_worker_pool_1_auto_scaling_max_count = "3"
 aks_worker_pool_1_auto_scaling_min_count = "1"
